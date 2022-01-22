834b8cf4c93a63781d410e4676b4847ec162d1fa
Sami Ahlfors <48211518+samiahlfors@users.noreply.github.com>
2022-01-22 15:02:52 +0100

    Update Coin.cs

--- Coin.cs
+++ Coin.cs
@@ -2,11 +2,13 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
-	private void OnTriggerEnter(Collider other)
-	{
-		// Make sure collision occurs with Player
-		if (!other.gameObject.CompareTag("Player")) return;
-
-		EventManager.OnCoinCollected(transform.position);
-	}
+    private void OnTriggerEnter(Collider other)
+    {
+        // Make sure collision occurs with Player
+        if (!other.gameObject.CompareTag("Player")) return;
+        
+        other.gameObject.GetComponent<PlayerHandler>().UpdateNumberOfCoins();
+        EventManager.OnCoinCollected(transform.position);
+        Destroy(gameObject);
+    }
 }
